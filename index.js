d9899b7a00e5bb459b84b0983849bf1707b29253
Desh Bagley <stemtrainer@gmail.com>
2022-03-25 10:47:22 -0400

    assignment done

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
+//put your code here
 
-// Write your code here
+//multiply variable test
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+//random variable test
+let random = Math.floor(Math.random() * 10) + 1;
+
+//mod equation
+
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+
+//max equation
+let max = Math.max(1, 2, 3, 20, 5);
